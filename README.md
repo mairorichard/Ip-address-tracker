40a869619deb3919f357ecafce46ab97ee577fad
mairorichard <nwokoyeoghenemairo@gmail.com>
2023-11-07 17:24:25 +0100

    Base UI

--- README.md
+++ README.md
@@ -1,8 +1,49 @@
-# React + Vite
+# Frontend Mentor - IP address tracker solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
 
-Currently, two official plugins are available:
+## Table of contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Overview](#overview)
+- [Get started](#get-started)
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+
+## Overview
+
+This project was achieved with React + Vite and Tailwind CSS
+
+## Get started
+
+To get started after cloning, run the following;
+
+- cd into root folder
+- npm install
+- npm run dev
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- React + Vite
+- Tailwind CSS
+
+<!-- end -->
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Info from "./components/Info";
+import Map from "./components/Map";
+import Large from "./assets/pattern-bg-desktop.png";
+import Small from "./assets/pattern-bg-mobile.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative grid place-items-center ">
+      {/* images */}
+      {/* <div className="absolute inset-0 z-0">
+        <div className="absolute inset-0">
+          <img src={Large} alt="" className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute inset-0">
+          <img src={Small} alt="" className="w-full h-full object-cover" />
+        </div>
+      </div> */}
+      {/* /images */}
+      <div className="z-10">
+        <h1 className="text-3xl font-medium mt-10 text-center">
+          IP Address Tracker
+        </h1>
+
+        <div className="mt-10">
+          <div className="">
+            <Info />
+          </div>
+          <div className="">
+            <Map />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
